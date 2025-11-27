2de4c1f7d5a1750d1bac187a7a4ce2efc0c84167
zak-dev-art <zachndungu861@gmail.com>
2025-11-27 21:44:22 +0300

    modified files

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,33 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if type(value) is not int:
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price cannot be negative")
+        self._price = value
+
+    def apply_discount(self, percent):
+        self.price *= (1 - percent/100)
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
-#!/usr/bin/env python3
+# lib/shoe.py
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self._condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    @property
+    def condition(self):
+        return self._condition
+
+    @condition.setter
+    def condition(self, value):
+        self._condition = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self._condition = "New"
